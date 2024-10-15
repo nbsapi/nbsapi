5cb5fd75f1cc5e6775d8928bfa090264f575c877
Stephan Hügel <shugel@tcd.ie>
2024-10-15 13:19:55 +0100

    Add endpoints and verification logic

--- src/nbsapi/utils/auth.py
+++ src/nbsapi/utils/auth.py
@@ -1,10 +1,16 @@
+from datetime import datetime, timedelta, timezone
+
 import jwt
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from nbsapi.api.dependencies.core import DBSessionDep
 from nbsapi.config import settings
+from nbsapi.models import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -16,15 +22,32 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def is_authenticated(user, password: str) -> bool:
-    if not user or not user.hashed_password:
+async def is_authenticated(db_session: AsyncSession, username: str, password: str):
+    user = (
+        await db_session.execute(select(User).where(User.username == username))
+    ).scalar_one_or_none()
+
+    if not user:
         return False
     if not verify_password(password, user.hashed_password):
         return False
-    return True
+    return user
 
 
 def decode_jwt(token: str) -> dict:
     return jwt.decode(
         token, ACCESS_TOKEN_SECRET_KEY, algorithms=[ACCESS_TOKEN_ALGORITHM]
     )
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=ACCESS_TOKEN_ALGORITHM
+    )
+    return encoded_jwt
