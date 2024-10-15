387ab199021f8da47e730f6eb900b0b963ed3d63
Stephan Hügel <shugel@tcd.ie>
2024-10-15 14:16:23 +0100

    Simplify user model

--- src/nbsapi/models/user.py
+++ src/nbsapi/models/user.py
@@ -8,9 +8,8 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     username: Mapped[str] = mapped_column(index=True, unique=True)
-    slug: Mapped[str] = mapped_column(index=True, unique=True)
     email: Mapped[str] = mapped_column(index=True, unique=True)
     first_name: Mapped[str]
     last_name: Mapped[str]
     hashed_password: Mapped[str]
-    is_superuser: Mapped[bool] = mapped_column(default=False)
+    disabled: Mapped[bool]
