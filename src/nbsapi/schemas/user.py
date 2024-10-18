1a3c40cb5c2d999ea5f4135107b18ae804e7644b
Stephan Hügel <shugel@tcd.ie>
2024-10-18 13:14:09 +0100

    make disabled field non-public, add User examples

--- src/nbsapi/schemas/user.py
+++ src/nbsapi/schemas/user.py
@@ -1,18 +1,18 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class User(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    username: str
-    email: str
-    first_name: str
-    last_name: str
-    disabled: bool
+    username: str = Field(None, examples=["nbsapi_user"])
+    email: str = Field(None, examples=["user@domain.com"])
+    first_name: str = Field(None, examples=["Jane"])
+    last_name: str = Field(None, examples=["Doe"])
 
 
 class UserWrite(User):
-    password: str
+    password: str = Field(None, examples=["nbsapi"])
+    disabled: bool = Field(False, examples=[False])
 
 
 class UserPrivate(User):
