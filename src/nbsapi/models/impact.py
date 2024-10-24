73bd623ab5d3919d5eb3963edd0d753eaa2c8633
Stephan Hügel <shugel@tcd.ie>
2024-10-24 15:33:34 +0100

    Add adaptation impact and related DB models

---
+++ src/nbsapi/models/impact.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from geoalchemy2 import Geometry, WKBElement
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from . import Base
+
+
+class Impact(Base):
+    __tablename__ = "impact"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(index=True)
+    magnitude: Mapped[float] = mapped_column(index=True, unique=False)
+    intensity_id: Mapped[int] = mapped_column(ForeignKey("impact_intensity.id"))
+    intensity: Mapped["ImpactIntensity"] = relationship(
+        back_populates="impacts", lazy="joined"
+    )
+    unit_id: Mapped[int] = mapped_column(ForeignKey("impact_unit.id"))
+    unit: Mapped["ImpactUnit"] = relationship(back_populates="impacts", lazy="joined")
+    solution: Mapped["NatureBasedSolution"] = relationship(back_populates="impact")
